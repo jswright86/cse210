a6608f6f90f742614d153acb31ff84a61dfeaedb
jswright86 <jwright9@byupathway.edu>
2025-06-30 21:35:13 -0500

    Completed Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        Console.Write("What is you grade percentage?");
+        string grade = Console.ReadLine();
+        int num = int.Parse(grade);
+
+        if (num >= 90)
+
+        {
+            grade = "A";
+        }
+        else if (num >= 80)
+        {
+            grade = "B";
+        }
+        else if (num >= 70)
+        {
+            grade = "C";
+        }
+        else if (num >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        if (grade == "F")
+        {
+            Console.Write($"Sorry you recieved an {grade}. Don't give up! Try again next semester.");
+        }
+        else if (grade == "A")
+        {
+            Console.Write($"Congratulations! You Passed with an {grade}!");
+        }
+        else
+        {
+            Console.Write($"Congratulations! You Passed with a {grade}!");
+        }
+        
+        } }
+       
