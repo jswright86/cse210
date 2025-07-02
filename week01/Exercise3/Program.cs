33a6008554b60a7a39193a1f89629d401ba7625d
jswright86 <jwright9@byupathway.edu>
2025-07-02 13:24:11 -0500

    Completed Exercise 3 on Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        /* Question 1 & 2
+        Console.WriteLine("What is the magic number?");
+        string number = Console.ReadLine();
+        int MagNum = int.Parse(number);
+
+        Completed with Question 3*/
+        Random randomGenerator = new Random();
+        int MagNum = randomGenerator.Next(1, 100);
+        int Guess = -1;
+        while (MagNum != Guess)
+        {
+            Console.WriteLine("What is your guess?");
+            string textGuess = Console.ReadLine();
+            Guess = int.Parse(textGuess);
+        
+            if (Guess > MagNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (Guess < MagNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }  
     }
-}
+} 
