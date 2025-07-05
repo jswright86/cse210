c180a679e83d4741f270a2ab29c70a656b357fe9
jswright86 <jwright9@byupathway.edu>
2025-07-05 15:13:37 -0500

    completed exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter you favorite number:");
+        string textfavnum = Console.ReadLine();
+        int number = int.Parse(textfavnum);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int sum = number * number;
+        return sum;
+    }
+    static void DisplayResult(string name, int sum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sum}");
+    }
+    
 }
+    
